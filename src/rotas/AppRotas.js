5fe312d98013e7ea55e89800e00fe122cd2cc0bb
Flavio Rogerio <solebac@hotmail.com>
2023-05-31 02:18:45 -0300

    Configure screenOptions Tab Navigation

--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import Home from '../../src/telas/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ProdutorRotas from './ProdutorRotas';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
+import Coracao from '../assets/coracao.svg';
+import Home from '../assets/home.svg';
 const Tab = createBottomTabNavigator();
 
 export default function AppRotas() {
   return (
     <>
       <NavigationContainer>
-        <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            headerShown: false,
+            tabBarIcon: ({color}) => {
+              let Icon = Home;
+              if (route.name === 'Melhores Produtores') {
+                Icon = Coracao;
+              }
+              return <Icon color={color} />;
+            },
+            tabBarActiveTintColor: '#2a9f85',
+            tabBarInactiveTintColor: '#c7c7c7',
+          })}>
           <Tab.Screen name="Home" component={ProdutorRotas} />
           <Tab.Screen
             name="Melhores Produtores"
